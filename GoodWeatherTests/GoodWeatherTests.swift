4e7a0b5bd1306d905ac4304815bef1e027165234
Eros Campos Guardia <eroscamposg@gmail.com>
2020-07-26 17:24:12 -0500

    Beginning Added Tests

---
+++ GoodWeatherTests/GoodWeatherTests.swift
@@ -0,0 +1,33 @@
+//
+//  GoodWeatherTests.swift
+//  GoodWeatherTests
+//
+//  Created by Eros Campos on 7/26/20.
+//  Copyright © 2020 Eros Campos. All rights reserved.
+//
+
+import XCTest
+
+class GoodWeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
